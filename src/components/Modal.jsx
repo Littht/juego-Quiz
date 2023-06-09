b0b546676de226e5f35a7be9dcc9af6574dfd488
Leo <leorbenavidesm@gmail.com>
2023-06-09 15:22:06 -0400

    agregar preguntas, estilos al reloj y colocar mensaje cuando ganas

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,6 +6,7 @@ export const Modal = () => {
     const respuestaCorrecta = useSelector((state) => state.funcionamiento.respuestaCorrecta)
     const contador = useSelector((state) => state.funcionamiento.contador)
     const tiempo = useSelector((state) => state.funcionamiento.tiempoState)
+    const juegoTerminado = useSelector((state) => state.funcionamiento.juegoTerminado)
     const dispatch = useDispatch() 
   return (
     <div className={styles.container}>
@@ -13,18 +14,25 @@ export const Modal = () => {
           respuestaCorrecta == 1 ? (
             <div className={styles.container_modal}>
                 <div className={styles.box}>
-                    <label className={styles.message} >Bien respondido, Selecciona avanzar si deseas seguir</label>
-                    <button className={styles.button} onClick={() => dispatch(imprimirPreguntas(), dispatch(playReloj()))}>Avanzar</button>
+                  <label className={styles.message} >Bien respondido, Selecciona avanzar si deseas seguir</label>
+                  <button className={styles.button} onClick={() => dispatch(imprimirPreguntas(), dispatch(playReloj()))}>Avanzar</button>
                 </div>
             </div>
           ) : contador == -1 || tiempo == 0 ? (
             <div className={styles.container_modal}>
                 <div className={styles.box}> 
-                    <label className={styles.message} htmlFor="">Maldito bruto,dale a inicio, peldite</label>
-                    <button className={styles.button} onClick={() => dispatch(resetearContador())}>Inicio</button>
+                  <label className={styles.message} htmlFor="">Maldito bruto,dale a inicio, peldite</label>
+                  <button className={styles.button} onClick={() => dispatch(resetearContador())}>Inicio</button>
                 </div>
             </div>
-          ):(
+          ): juegoTerminado == true ? (
+            <div className={styles.container_modal}>
+              <div className={styles.box}> 
+                <label className={styles.message} htmlFor="">Felicidades mmgvo</label>
+                <button className={styles.button} onClick={() => dispatch(resetearContador())}>Inicio</button>
+              </div>
+            </div>
+          ) : (
             <div></div>
           )
         }
--- src/features/funcionamiento/funcionamientoSlice.js
+++ src/features/funcionamiento/funcionamientoSlice.js
@@ -2,13 +2,14 @@ import { createSlice } from '@reduxjs/toolkit'
 import preguntas from '../../preguntas';
 
 const initialState = {
-  contador: 0,
+  contador: 0,// cuando es 0 o más para avanzar, -1 cuando respondes mal
   preguntaActual: null,
   preguntasRespondidas: [],
   respuestaCorrecta:0, // 0 cuando es neutro, 1 cuando es correcta
   tiempoState: 1, // 0 cuando se acaba. 1 cuando el reloj tiene tiempo
-  keyReloj:0,
-  correrReloj:true,
+  keyReloj:0, //esto es para pausar el reloj
+  correrReloj:true, //esto es para pausarlo y que siga corriendo el tiempo
+  juegoTerminado:false,
 }
 
 const preguntasSlice = createSlice({
@@ -17,7 +18,8 @@ const preguntasSlice = createSlice({
   reducers: {
     imprimirPreguntas: (state, action) => {
       state.respuestaCorrecta = 0
-      if (state.contador === 0) {
+      console.log(state.contador,"contador")
+      if (state.contador <= 4) {
         const preguntasDisponibles = preguntas.nivel_1.filter(
           pregunta => !state.preguntasRespondidas.includes(pregunta.pregunta)
         );
@@ -25,7 +27,7 @@ const preguntasSlice = createSlice({
         const preguntaAleatoria = preguntasDisponibles[indiceAleatorio];
         state.preguntaActual = preguntaAleatoria;
         state.preguntasRespondidas.push(preguntaAleatoria.id);
-      } else if (state.contador === 1) {
+      } else if (state.contador >= 5 && state.contador <=9) {
         const preguntasDisponibles = preguntas.nivel_2.filter(
           pregunta => !state.preguntasRespondidas.includes(pregunta.pregunta)
         );
@@ -33,6 +35,9 @@ const preguntasSlice = createSlice({
         const preguntaAleatoria = preguntasDisponibles[indiceAleatorio];
         state.preguntaActual = preguntaAleatoria;
         state.preguntasRespondidas.push(preguntaAleatoria.id);
+      }else{
+        state.juegoTerminado = true
+        state.preguntaActual = null
       }
     },
     seleccionarRespuesta : (state, action) =>{
@@ -61,6 +66,7 @@ const preguntasSlice = createSlice({
     resetearContador : (state) => {
       state.contador = 0
       state.tiempoState = 1
+      state.juegoTerminado = false
     }
   },
 });
